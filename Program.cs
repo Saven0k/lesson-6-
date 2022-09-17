94d24cacf6ae474a5eb46cab8f77859d4cd5a23a
Savenok <Romasav2017@gmail.com>
2022-09-17 18:42:39 +0300

    44

--- Program.cs
+++ Program.cs
@@ -1,20 +1,35 @@
-﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+﻿//Задача 44: Не используя рекурсию,выведете первые N чисел Фибоначчи .Первые два числа Фибоначчи : 0 , 1.
 Console.Clear();
-Console.Write("Введите число: ");
-long a = long.Parse(Console.ReadLine());
-Console.Clear();
-Console.Write($"{a} ->");
-long ten_in_two(long number)
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+ulong[] Fibonachi(int number)
 {
-    long result = 0;
-    if (number > 0)
+    int temp = 2;
+    ulong[] result = new ulong[number];
+    result[0] = 0;
+    result[1] = 1;
+    Console.Write($"Если N = {number}  - > {result[0]}  {result[1]} ");
+    while (temp < number)
     {
-        result = number % 2;
-        number = number /2 ;
-        Console.Write($" {result} ");
-        number = ten_in_two(number);
+        result[temp] = result[temp - 1] + result[temp - 2];
+        Console.Write($" {result[temp]} ");
+        temp++;
     }
-    else{}
     return result;
 }
-ten_in_two(a);
+
+ulong MaxInFibonachi(ulong[] arra)
+{
+    ulong max = 0;
+    ulong min = arra[0];
+    for (int g = 0; g < arra.Length; g++)
+    {
+        if (min > arra[g]) min = arra[g];
+        else max = arra[g];
+    }
+    Console.WriteLine($"  Максимальное число-> {max}");
+    return max;
+}
+ulong[] a = Fibonachi(N);
+Console.WriteLine("      ");
+MaxInFibonachi(a);
