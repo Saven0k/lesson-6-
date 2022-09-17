5d537326878fc6dc7b29b553df8f2a3a06523ccd
Savenok <Romasav2017@gmail.com>
2022-09-17 16:29:31 +0300

    40

--- Program.cs
+++ Program.cs
@@ -1,58 +1,17 @@
-﻿//Зачада 39
-
+﻿// Задача   40: Напишите программу,которая принимает на вход три числа и проверяет,может ли существовать треугольник с такими сторонами.
 Console.Clear();
-int[] GetArray(int size, int minValue, int maxValue)
+Console.WriteLine("Введите длинны сторон: ");
+int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+void if_this_triangle(int[] arra)
 {
-    int[] res = new int[size];
-    Console.Write("[ ");
-    for (int i = 0; i < size; i++)
+    if (arra.Length > 3) Console.WriteLine("Вы ввели больше чем 3 числа");
+    else
     {
-        res[i] = new Random().Next(minValue, maxValue + 1);
-        if (i == size - 1) Console.Write($"{res[i]}");
-        else Console.Write($"{res[i]} , ");
+        if (arra[0] + arra[1] <= arra[2] || arra[0] + arra[2] <= arra[1] || arra[1] + arra[2] <= arra[0]) Console.WriteLine($"Треугольника со сторонами {arra[0]}, {arra[1]}, {arra[2]} - не существует");
+        else Console.WriteLine($"Треугольника со сторонами {arra[0]}, {arra[1]}, {arra[2]} - существует");
     }
-    Console.Write("]");
-    return res;
+
 }
+if_this_triangle(array);
 
-void PalindromArray(int[] arra)
-{
-    Console.Write(" -> [ ");
-    for (int i = 0; i < arra.Length; i++)
-    {
-        if (arra[i] == arra[0])
-        {
-            int s1 = arra[i];
-            arra[i] = arra[arra.Length - 1];
-            arra[arra.Length - 1] = s1;
-        }
-        else if (arra[i] == arra[1])
-        {
-            int s2 = arra[i];
-            arra[i] = arra[arra.Length - 2];
-            arra[arra.Length - 2] = s2;
-        }
-        else { }
-        if (arra[i] == arra[arra.Length - 1]) Console.Write($"{arra[i]}");
-        else Console.Write($"{arra[i]} , ");
-    }
-    Console.Write("]");
-}
-
-void ReversArray(int[] arra)
-{
-    for (int i = 0; i < arra.Length / 2; i++)
-    {
-        int s3 = arra[i];
-        arra[i] = arra[arra.Length - i - 1];
-        arra[arra.Length - i - 1] = s3;
-    }
-}
-int[] array = GetArray(10, 0, 10);
-ReversArray(array);
-Console.Write(" -> [ ");
-Console.Write(String.Join(" , " ,  array));
-Console.Write("]  ");
-
-
-
+// int[] vosko = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
